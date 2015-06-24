337b3c3b822ce7bca1e53cf589272660a7e05ec5
monkeyde17 <monkey_tv@126.com>
2015-06-24 23:43:13 +0800

    fix some problem

--- tinypng.py
+++ tinypng.py
@@ -26,69 +26,83 @@ class shrink_thread(threading.Thread):
         self.path = path
 
     def run(self):
-        tmppath = outputdir + os.sep + self.path 
-        if not os.path.isdir(tmppath):
-            os.makedirs(tmppath)
+        tmppath = os.path.join(outputdir, self.path)
+        if not os.path.exists(tmppath):
+            try:
+                os.makedirs(tmppath)
+            except Exception as e:
+                pass
 
-        inputfile = self.path + os.sep + self.filename
-        outputfile = tmppath + os.sep + self.filename
-        
-        request = Request("https://api.tinypng.com/shrink", open(inputfile, "rb").read())
-
-        # add the auth
-        auth = b64encode(bytes("api:" + key)).decode("ascii")
-        request.add_header("Authorization", "Basic %s" % auth)
-
-        # http post request
-        response = urlopen(request)
+        #inputfile = self.path + os.sep + self.filename
+        inputfile = os.path.join(self.path, self.filename)
+        #outputfile = tmppath + os.sep + self.filename
+        outputfile = os.path.join(tmppath, self.filename)
 
         global isize
         global osize
         global filecount
 
-        if response.getcode() == 201:
-            # get the json result
-            rejson = json.loads(response.read())
+        if not os.path.exists(outputfile):
+            print "The input file : " + inputfile
+            request = Request("https://api.tinypng.com/shrink", open(inputfile, "rb").read())
 
-            # get the url
-            result = urlopen(rejson["output"]["url"]).read()
+            # add the auth
+            auth = b64encode(bytes("api:" + key)).decode("ascii")
+            request.add_header("Authorization", "Basic %s" % auth)
 
-            print "The output file : " + outputfile
-            open(outputfile, "wb").write(result)
+            # http post request
+            response = urlopen(request)
 
-            isize += rejson["input"]["size"]
-            osize += rejson["output"]["size"]
+
+            if response.getcode() == 201:
+                # get the json result
+                rejson = json.loads(response.read())
+
+                # get the url
+                result = urlopen(rejson["output"]["url"]).read()
+
+                print "The output file : " + outputfile
+                open(outputfile, "wb").write(result)
+
+                isize += rejson["input"]["size"]
+                osize += rejson["output"]["size"]
+            else:
+                # Something went wrong! You can parse the JSON body for details.
+                print "Error Code : " + response.getcode()
+                print "Compression failed"
 
         else:
-            # Something went wrong! You can parse the JSON body for details.
-            print "Error Code : " + response.getcode()
-            print "Compression failed"
+            pass
+            #print "_______________Exists File______________ " + inputfile
 
         # end the thread
-        self.thread_stop = True
+        #self.thread_stop = True
+        #filecount = filecount - 1
+        self.stop()
 
-        filecount = filecount - 1
-
-        if filecount == 0:
-            print "======================================"
-            print "input size : " + str(isize)
-            print "output size : " + str(osize)
-            print "rate : " + str(1.0 * osize / isize)
+        #if filecount <= 0:
+            #print "====================================="
+            #print "input size : " + str(isize)
+            #print "output size : " + str(osize)
+            #print "rate : " + str(1.0 * osize / isize)
            
     def stop(self):
+        global filecount
         self.thread_stop = True
+        filecount = filecount - 1
 
 def shrink_png_by_path(path):
     global filecount
     if os.path.isdir(path):
-        print "current path : " + path
+        #print "current path : " + path
         for curfile in os.listdir(path):
-            nextpath = path + os.sep + curfile
+            #nextpath = path + os.sep + curfile
+            nextpath = os.path.join(path, curfile)
             if os.path.isdir(nextpath):
                 shrink_png_by_path(nextpath)
-            elif curfile.find(".png") != -1 or curfile.find(".jpg") != -1:
+            elif curfile.lower().endswith(".png") or curfile.lower().endswith(".jpg"):
                 filecount = filecount + 1
-                print "the input file : " + path + os.sep + curfile
+                #print "the input file : " + nextpath
                 shrink_thread(path, curfile).start()
                 #time.sleep(0.1)
     else:
